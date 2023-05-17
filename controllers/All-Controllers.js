352cbe319d2c6c492c48c91f09de5e278cb5d216
AbhishekAK411 <127575574+AbhishekAK411@users.noreply.github.com>
2023-05-17 12:23:03 +0530

    express.js addition and how to create a listener
    importing express into index.js and using the express() function to create an app that listens to port 3000.

---
+++ controllers/All-Controllers.js
@@ -0,0 +1,7 @@
+export const abhishekRoute = (req,res) => {
+    res.send("Hello World abhishek");
+}
+
+export const homeRoute = (req,res) => {
+    res.send("Home Route");
+}
--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-console.log("Hello World!");
-console.log("Abhishek");
+import express from "express";
+import {abhishekRoute,homeRoute} from './controllers/All-Controllers.js'
+
+const app = express();
+
+app.get('/', homeRoute);
+app.get('/abhishek', abhishekRoute);
+
+
+app.listen(3000);
