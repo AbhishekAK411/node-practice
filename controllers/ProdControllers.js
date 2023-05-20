9cd8f252265a99a116f9936b9ea0c8c6cdcfc76a
AbhishekAK411 <127575574+AbhishekAK411@users.noreply.github.com>
2023-05-20 13:10:21 +0530

    commit

--- controllers/ProdControllers.js
+++ controllers/ProdControllers.js
@@ -1,7 +1,7 @@
 export const addProduct = (req,res) => {
     try{
         console.log(req.body);
-        res.send("add Product function");
+        return res.send("add Product function");
     } catch(error){
         console.log(error,"add-product error");
     }
